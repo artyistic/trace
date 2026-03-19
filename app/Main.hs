97a5b10179bc00c1b425340fbb8824db79925b2b
artyistic <artyistic@gmail.com>
2026-03-19 00:14:47 -0400

    added selection for scenes and merged camera configs together with scenes

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Camera
 import Scenes
-import Graphics
 import Render
 import Control.Monad.Random
 import System.Environment (getArgs)
@@ -10,36 +9,36 @@ import System.Exit (exitFailure)
 import Text.Read (readMaybe)
 
 data RenderConfig = RenderConfig
-  { rcImageWidth      :: Int,
+  { rcSceneIndex    :: Maybe Int,  -- Nothing = prompt user
+    rcImageWidth    :: Int,
     rcSamplesPerPixel :: Int,
-    rcNumBounces      :: Int
+    rcNumBounces    :: Int
   }
 
 parseArgs :: [String] -> Either String RenderConfig
 parseArgs [w, s, b] = case (readMaybe w, readMaybe s, readMaybe b) of
-  (Just w', Just s', Just b') -> Right $ RenderConfig w' s' b'
+  (Just w', Just s', Just b') -> Right $ RenderConfig Nothing w' s' b'
   _                           -> Left "All arguments must be integers"
-parseArgs _ = Left "Usage: tracerays <width> <samplesPerPixel> <numBounces>"
+parseArgs [sc, w, s, b] = case (readMaybe sc, readMaybe w, readMaybe s, readMaybe b) of
+  (Just sc', Just w', Just s', Just b')
+    | sc' < 0 || sc' >= length scenes -> Left $ "Scene index out of range (0-" ++ show (length scenes - 1) ++ ")"
+    | otherwise -> Right $ RenderConfig (Just sc') w' s' b'
+  _ -> Left "All arguments must be integers"
+parseArgs _ = Left "Usage: tracerays [sceneIndex] <width> <spp> <bounces>"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  cfg  <- case parseArgs args of
+  args  <- getArgs
+  cfg   <- case parseArgs args of
     Left err -> putStrLn err >> exitFailure
     Right c  -> return c
-
-  gen <- getStdGen
-  let world = evalRand bigWorld gen
-      cam   = makeCamera defaultCameraConfig
-        { cfgAspectRatio     = 16.0 / 9.0,
-          cfgVfov            = 20,
-          cfgLookFrom        = V3 13 2 3,
-          cfgLookAt          = V3 0 0 0,
-          cfgVup             = V3 0 1 0,
-          cfgDefocusAngle    = 0.6,
-          cfgFocusDistance   = 10.0,
-          cfgImageWidth      = rcImageWidth cfg,
+  scene <- case rcSceneIndex cfg of
+    Just i  -> return (scenes !! i)
+    Nothing -> selectScene
+  gen   <- getStdGen
+  let world = evalRand (sceneWorld scene) gen
+      cam   = makeCamera (sceneCamera scene)
+        { cfgImageWidth      = rcImageWidth cfg,
           cfgSamplesPerPixel = rcSamplesPerPixel cfg
         }
-
   render "./output/test.ppm" world cam (rcNumBounces cfg)
--- src/Scenes.hs
+++ src/Scenes.hs
@@ -1,11 +1,95 @@
-module Scenes where
+module Scenes
+  ( Scene (..),
+    scenes,
+    selectScene
+  )
+where
 
+import Camera
 import Control.Monad.Random
-import Shapes.Sphere
-import Random
-import Data.Maybe
+import Data.Maybe (catMaybes)
 import Graphics
 import Hittable
+import Random
+import Shapes.Sphere
+import System.IO (hFlush, stdout)
+import Text.Read (readMaybe)
+
+data Scene = Scene
+  { sceneName        :: String,
+    sceneDescription :: String,
+    sceneWorld       :: Rand StdGen [Hittable],
+    sceneCamera      :: CameraConfig
+  }
+
+scenes :: [Scene]
+scenes =
+  [ Scene
+      "bouncing-spheres"
+      "Random spheres with motion blur and depth of field"
+      bigWorld
+      defaultCameraConfig
+        { cfgAspectRatio     = 16.0 / 9.0,
+          cfgImageWidth      = 400,
+          cfgSamplesPerPixel = 100,
+          cfgVfov            = 20,
+          cfgLookFrom        = V3 13 2 3,
+          cfgLookAt          = V3 0 0 0,
+          cfgVup             = V3 0 1 0,
+          cfgDefocusAngle    = 0.6,
+          cfgFocusDistance   = 10.0
+        },
+    Scene
+      "dielectric"
+      "Glass bubble and metal sphere test"
+      (pure dielectricTestWorld)
+      defaultCameraConfig
+        { cfgAspectRatio     = 16.0 / 9.0,
+          cfgImageWidth      = 400,
+          cfgSamplesPerPixel = 100,
+          cfgVfov            = 20,
+          cfgLookFrom        = V3 (-2) 2 1,
+          cfgLookAt          = V3 0 0 (-1),
+          cfgVup             = V3 0 1 0,
+          cfgDefocusAngle    = 10.0,
+          cfgFocusDistance   = 3.4
+        },
+    Scene
+      "vfov-test"
+      "Wide angle FOV test with two colored spheres"
+      (pure vfovTestWorld)
+      defaultCameraConfig
+        { cfgAspectRatio     = 16.0 / 9.0,
+          cfgImageWidth      = 400,
+          cfgSamplesPerPixel = 100,
+          cfgVfov            = 90,
+          cfgLookFrom        = V3 0 0 0,
+          cfgLookAt          = V3 0 0 (-1),
+          cfgVup             = V3 0 1 0,
+          cfgDefocusAngle    = 0,
+          cfgFocusDistance   = 10.0
+        }
+  ]
+
+selectScene :: IO Scene
+selectScene = do
+  putStrLn "Available scenes:"
+  mapM_
+    (\(i, s) ->
+        putStrLn $
+          "  " ++ show i ++ ": " ++ sceneName s
+            ++ " — "
+            ++ sceneDescription s
+    )
+    (zip [(0 :: Int) ..] scenes)
+  putStr "Select scene: "
+  hFlush stdout
+  input <- getLine
+  case readMaybe input of
+    Just i | i >= 0 && i < length scenes -> return (scenes !! i)
+    _ -> putStrLn "Invalid selection, try again." >> selectScene
+
+-- scene definitions
 
 dielectricTestWorld :: [Hittable]
 dielectricTestWorld =
@@ -14,57 +98,51 @@ dielectricTestWorld =
       materialLeft   = mkDielectric 1.5
       materialRight  = mkMetal (color 0.8 0.6 0.2) 1.5
       materialBubble = mkDielectric (1.00 / 1.50)
-
-      ground     = stationarySphere (V3 0.0 (-100.5) (-1.0)) 100.0 materialGround
-      center     = stationarySphere (V3 0.0 0.0 (-1.2)) 0.5 materialCenter
-      right      = stationarySphere (V3 1.0 0.0 (-1.0)) 0.5 materialRight
-      left       = stationarySphere (V3 (-1.0) 0.0 (-1.0)) 0.5 materialLeft
-      leftBubble = stationarySphere (V3 (-1.0) 0.0 (-1.0)) 0.4 materialBubble
+      ground         = stationarySphere (V3 0.0 (-100.5) (-1.0)) 100.0 materialGround
+      center         = stationarySphere (V3 0.0 0.0 (-1.2)) 0.5 materialCenter
+      right          = stationarySphere (V3 1.0 0.0 (-1.0)) 0.5 materialRight
+      left           = stationarySphere (V3 (-1.0) 0.0 (-1.0)) 0.5 materialLeft
+      leftBubble     = stationarySphere (V3 (-1.0) 0.0 (-1.0)) 0.4 materialBubble
   in [ground, center, left, leftBubble, right]
 
 vfovTestWorld :: [Hittable]
 vfovTestWorld =
-  let r      = cos $ pi / 4
+  let r        = cos $ pi / 4
       matLeft  = mkLambertian $ color 1 0 0
       matRight = mkLambertian $ color 0 0 1
-      left   = stationarySphere (V3 (-r) 0 (-1)) r matLeft
-      right  = stationarySphere (V3 r 0 (-1)) r matRight
+      left     = stationarySphere (V3 (-r) 0 (-1)) r matLeft
+      right    = stationarySphere (V3 r 0 (-1)) r matRight
   in [left, right]
 
 bigWorld :: Rand StdGen [Hittable]
 bigWorld = do
-  let
-    matGround = mkLambertian $ color 0.5 0.5 0.5
-    matbS1    = mkDielectric 1.5
-    matbS2    = mkLambertian $ color 0.4 0.2 0.1
-    matbS3    = mkMetal (color 0.7 0.6 0.5) 0.0
-
-    ground      = stationarySphere (V3 0 (-1000) 0) 1000 matGround
-    bigSphere1  = stationarySphere (V3 0 1 0) 1.0 matbS1
-    bigSphere2  = stationarySphere (V3 (-4) 1 0) 1.0 matbS2
-    bigSphere3  = stationarySphere (V3 4 1 0) 1.0 matbS3
+  let matGround  = mkLambertian $ color 0.5 0.5 0.5
+      matbS1     = mkDielectric 1.5
+      matbS2     = mkLambertian $ color 0.4 0.2 0.1
+      matbS3     = mkMetal (color 0.7 0.6 0.5) 0.0
+      ground     = stationarySphere (V3 0 (-1000) 0) 1000 matGround
+      bigSphere1 = stationarySphere (V3 0 1 0) 1.0 matbS1
+      bigSphere2 = stationarySphere (V3 (-4) 1 0) 1.0 matbS2
+      bigSphere3 = stationarySphere (V3 4 1 0) 1.0 matbS3
   smallSpheres <- randomSpheres
   pure $ [ground, bigSphere1, bigSphere2, bigSphere3] ++ smallSpheres
 
 randomSpheres :: Rand StdGen [Hittable]
 randomSpheres =
-  let rSphere = [genSpheres a b | a <- [-11..11], b <- [-11..11]]
-      t       = sequenceA rSphere
-  in catMaybes <$> t
+  catMaybes <$> sequenceA [genSpheres a b | a <- [-11 .. 11], b <- [-11 .. 11]]
 
 genSpheres :: Int -> Int -> Rand StdGen (Maybe Hittable)
 genSpheres a b = do
   offsetX   <- getRandomDouble
   offsetZ   <- getRandomDouble
   chooseMat <- getRandomDouble
-
   let center = V3 (fromIntegral a + 0.9 * offsetX) 0.2 (fromIntegral b + 0.9 * offsetZ)
   if distance center (V3 4 0.2 0) <= 0.9
     then pure Nothing
-    else Just <$> t chooseMat center
+    else Just <$> mkSphere chooseMat center
   where
-    t :: Double -> V3 -> Rand StdGen Hittable
-    t chooseMat center
+    mkSphere :: Double -> V3 -> Rand StdGen Hittable
+    mkSphere chooseMat center
       | chooseMat < 0.8 = do
           c          <- colorFromV3 <$> getRandomVec 0 1
           center2Dir <- flip (V3 0) 0 <$> getRandomR (0, 0.5)
